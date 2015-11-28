513d64663cedb43671b392b791c6afc4367e4d70
Eyas <liuyuesongde@163.com>
2015-11-29 00:53:40 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/client/modules/blog/components/Detail/index.js
+++ app/client/modules/blog/components/Detail/index.js
@@ -9,6 +9,7 @@ export default class Detail extends React.Component {
   constructor(props) {
     super(props);
     this.props.getPost(this.props.params.id);
+    document.body(50);
   }
   render() {
     const { post } = this.props;

3cf7856fbe9914f3ac0d74528be769568bed41f7
eyas <eyas_liu@fmces.com>
2016-01-15 14:52:12 +0800

    shipit 部署

--- shipitfile.js
+++ shipitfile.js
@@ -1,11 +1,25 @@
+var path = require('path');
+
 module.exports = shipit => {
+  require('shipit-deploy')(shipit);
   shipit.initConfig({
+    default: {
+      workspace: path.join(__dirname, '../deploy'),
+      deployTo: '/home/eyas/www/default',
+      repositoryUrl: 'git@git.oschina.net:yuesong/startkit-app.git',
+      branch: 'master',
+      ignores: ['.git', 'node_modules'],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      shallowClone: true
+    },
     pro: {
+      deployTo: '/home/eyas/www/eyasliu.gthub.io',
       servers: 'eyas@startkit.eyasweb.com:3219'
     }
   });
 
-  shipit.task('fetch', () => {
-    return shipit.remote('cd /home/www/startkit && git clone git@git.oschina.net:yuesong/startkit-app.git');
-  });
+  // shipit.task('fetch', () => {
+  //   return shipit.remote('cd /home/www/startkit && git clone git@git.oschina.net:yuesong/startkit-app.git');
+  // });
 };

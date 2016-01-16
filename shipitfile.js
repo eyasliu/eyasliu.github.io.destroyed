f645989857a9ac6c727ef3a71c7f9843a9fd2049
Eyas <liuyuesongde@163.com>
2016-01-16 18:40:19 +0800

    shipit deploy

--- shipitfile.js
+++ shipitfile.js
@@ -1,25 +1,34 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = shipit => {
   require('shipit-deploy')(shipit);
+  shipit.releasesPath = '/home/eyas/www/default';
   shipit.initConfig({
     default: {
-      workspace: path.join(__dirname, './deploy'),
+      workspace: path.join('./deploy'),
+      // workspace: 'deploy',
       deployTo: '/home/eyas/www/default',
+      
+      releasePath: '/home/eyas/www/',
       repositoryUrl: 'git@git.oschina.net:yuesong/startkit-app.git',
       branch: 'master',
       ignores: ['.git', 'node_modules'],
       keepReleases: 2,
+      key: path.join('C:', 'Users/Eyas/.ssh', 'id_rsa'),
       deleteOnRollback: false,
       shallowClone: true
     },
     pro: {
       deployTo: '/home/eyas/www/eyasliu.gthub.io',
+      releasesPath: '/home/eyas/www/',
+      releasePath: '/home/eyas/www/',
+      // releaseDirname: 'eyasliu.gthub.io',
+      repositoryUrl: 'git@git.oschina.net:yuesong/eyasliu.github.io.git',
       servers: 'eyas@startkit.eyasweb.com:3219'
     }
   });
 
-  // shipit.task('fetch', () => {
-  //   return shipit.remote('cd /home/www/startkit && git clone git@git.oschina.net:yuesong/startkit-app.git');
-  // });
+  shipit.task('fetch', () => {
+    return shipit.remote('cd /home/www/startkit && ls -al');
+  });
 };
